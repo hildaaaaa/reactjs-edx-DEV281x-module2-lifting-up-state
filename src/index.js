b5b6b1a24eda34ed41c440bd61dfe48583980086
hilda <ylchungac@connect.ust.hk>
2019-04-13 17:00:21 +0800

    Remove src files and add index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Details extends React.Component {
+  render() {
+    return (
+      <h1>{this.props.details}</h1>
+    )
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Button extends React.Component {
+  render() {
+    return (
+      <button style={{color: this.props.active ? 'red' : 'blue'}} onClick={() => {this.props.clickHandler(this.props.id, this.props.name)}}>{this.props.name}</button>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {activeArray:[0,0,0,0],details: ""}
+    this.clickHandler = this.clickHandler.bind(this)
+  }
+  
+  clickHandler(id, name) {
+    var arr = [0,0,0,0]
+    arr[id] = 1
+    this.setState({activeArray: arr, details: name})
+  }
+  render() {
+    return (
+      <div>
+        <Button id={0} clickHandler={this.clickHandler} active={this.state.activeArray[0]} name="One"/>
+        <Button id={1} clickHandler={this.clickHandler} active={this.state.activeArray[1]} name="Two"/>
+        <Button id={2} clickHandler={this.clickHandler} active={this.state.activeArray[2]} name="Three"/>
+        <Button id={3} clickHandler={this.clickHandler} active={this.state.activeArray[3]} name="Four"/>
+        <Details details={this.state.details}/>
+      </div>
+    )
+  }
+}
+ReactDOM.render(
+  <App/>,
+  document.getElementById("root")
+)
